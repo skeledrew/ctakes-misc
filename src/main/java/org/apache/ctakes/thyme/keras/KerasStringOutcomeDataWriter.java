fc41557391fe5a41eb7d39528a9b7ed5b3228f4e
dimdym <dimdym@gmail.com>
2017-04-11 09:29:31 -0500

    creating a brand new repo

---
+++ src/main/java/org/apache/ctakes/thyme/keras/KerasStringOutcomeDataWriter.java
@@ -0,0 +1,40 @@
+package org.apache.ctakes.thyme.keras;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.apache.ctakes.temporal.keras.KerasStringOutcomeClassifierBuilder;
+import org.apache.ctakes.temporal.keras.ScriptStringFeatureDataWriter;
+import org.cleartk.ml.CleartkProcessingException;
+import org.cleartk.ml.util.featurevector.FeatureVector;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * <br>
+ * Copyright (c) 2016, Regents of the University of Colorado <br>
+ * All rights reserved.
+ * 
+ * @author Tim Miller
+ * @version 2.0.1
+ * 
+ */
+@Beta
+public class KerasStringOutcomeDataWriter extends ScriptStringFeatureDataWriter<KerasStringOutcomeClassifierBuilder>{
+
+	public KerasStringOutcomeDataWriter(File outputDirectory)
+			throws FileNotFoundException {
+		super(outputDirectory);
+	}
+
+	@Override
+	protected KerasStringOutcomeClassifierBuilder newClassifierBuilder() {
+		return new KerasStringOutcomeClassifierBuilder();
+	}
+
+	@Override
+	protected void writeEncoded(FeatureVector features, Integer outcome) throws CleartkProcessingException {
+		// TODO Auto-generated method stub
+
+	}
+}
